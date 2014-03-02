c0d1b32ca6db390cdef5047ad1daa1055c441b1a
greenteawarrior <emily.wang@students.olin.edu>
2014-03-02 16:58:56 -0500

    a simple list application

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/', routes.index_post);
+app.post('/delete/:id', routes.index_delete);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
+//storing all the bulletpoint entires of the list in an array 
+//(probably because mongoose/DB stuff would be overkill for this example)
+var things = [];
 
-/*
- * GET home page.
- */
+exports.index = function (req, res) {
+  res.render('index', {
+    title: 'greenteaweb',
+    things: things
+  });
+};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index_post = function (req, res) {
+  //push is a javascript function that adds new elements to the end of an array
+  things.push(req.body.saidsomething); 
+  res.redirect('/');
+};
+
+exports.index_delete = function (req, res) {
+  things.splice(parseInt(req.params.id), 1);
+  res.redirect('/');
 };
